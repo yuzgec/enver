4f5792afae21200580bf7a40820c3058045138f3
yuzgec <olcayy@gmail.com>
2023-12-13 15:45:33 +0300

    Fix

--- app/Http/Requests/SliderRequest.php
+++ app/Http/Requests/SliderRequest.php
@@ -6,25 +6,29 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class SliderRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
+  
+
+        public function authorize()
+        {
+            return true;
+        }
+    
+        public function rules()
+        {
+            return [
+                'title'                 => 'required|min:2|max:99|unique:faq,title,'.$this->id,
+            ];
+        }
+    
+        public function messages()
+        {
+            return [
+                'title.required'            => 'Slider başlığını giriniz',
+                'title.max'                 => 'Slider başlığı en fazla 99 karakter olabilir',
+                'title.min'                 => 'Slider başlığı en az 2 karakter olabilir',
+                'title.unique'              => 'Slider başlığı daha önce eklenmiş',
+               
+            ];
+        }
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
-}
